3012c0ca1adb977114aa06d46ae49956dfb4bbbf
Caprikey <DantyDCook@gmail.com>
2025-01-27 02:12:14 -0500

    updates

--- LaunchVEnvAndLabServer.ps1
+++ LaunchVEnvAndLabServer.ps1
@@ -1,19 +1,73 @@
-# Navigate to the directory containing the virtual environment
+# Verify Virtual Env module is installed 
+python -m pip install virtualenv
+
+# Backup One Directory For Virtual Env
 cd ".."
+
+# Test If Virtual Env directory exists
+if (-Not (Test-Path -Path "\virtual_envs\")) {
+
+    Write-Host "Directory does not exist."
+    Write-Host "Running First Time Initialization" 
+
+    # Creating Virtual Environment Folder
+    New-Item -ItemType Directory -Force -Path virtual_envs
+    #New-Item -ItemType Directory -Force -Path Test
+    
+    #Create a Virtual Env
+    python -m venv virtual_envs/D497_Project_1_venv
+    #python -m venv Test/D497_Project_Test_venv
+
+}
 
 # Activate the virtual environment
 .\\virtual_envs\D497_Project_1_venv\Scripts\Activate.ps1
+#.\\Test\D497_Project_Test_venv\Scripts\Activate.ps1
+
+# Install Python Requirements
+python -m pip install -r requirements.txt
 
 # Change Directories.
 cd "WGU_D497_Project_1"
 
+# Install Custom Module
+python -m pip install -e .
+
+# Run Folder_Manger.py file to setup folder_paths and create any missing directories. 
+python ./d497_helpers/folder_manager.py
+
 # Launch a new powershell window and run the Launch Virtual Environment Script and keep window open. 
 Start-Process powershell -ArgumentList "-NoExit", "-File", "launchvenv.ps1"
 
-# Launch VSCode
-code .
+# Check if VS Code is installed, If true, launch VS Code from current directory, if false, do not launch vs code
+if ((Get-Process -Name code) -ne $null) { 
 
-# Launch Jupyterlab
-python -m jupyterlab --ip 192.168.86.70 --port 8888 --notebook-dir=.
+    Write-Output "VS Code is installed" 
+
+    Write-Output "Launching VS Code in working directory"
+    
+    code . 
+    
+} else {
+    Write-Output "VS Code is not installed" 
+
+    Write-Output "Skipping VS Code Launch"
+
+}
 
 
+# Get Computer's IP Address and save to a variable 
+$IpAddress = (Get-NetIPAddress |
+  Where-Object {
+    $_.AddressState -eq 'Preferred' -and 
+    $_.ValidLifetime -lt '24:00:00'
+  }
+).IPAddress
+
+# Creating an expression to be involved to launch the juptyerlab to a specific page with ip, port, and notebook directory parameters set. 
+$expression = "python -m jupyterlab notebooks/order_of_operations_landing.ipynb --ip $($IpAddress[1]) --port 8888 --notebook-dir=."
+
+# Invoking expression above. 
+Invoke-Expression $expression
+
+#python -m jupyterlab notebooks/order_of_operations_landing.ipynb --ip $($IpAddress[1]) --port 8888 --notebook-dir=.
